33884915fdf94a1cce8c4ecfec65d7bc93e1646b
Ahmed Hossam <ahmedhossam9226@gmail.com>
2024-09-14 06:55:28 +0300

    Time: 55 ms (76.72%) | Memory: 49.7 MB (93.22%) - LeetSync

---
+++ 136-single-number/single-number.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const singleNumber = (nums = []) => {
+  let single = 0;
+  
+  for (let i = 0; i < nums.length; i++) {
+    single ^= nums[i];
+  }
+
+  return single;
+};
