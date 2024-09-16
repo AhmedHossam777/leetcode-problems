5cbd7f233728d8d2815c4e332b3b4dc337f9a663
Ahmed Hossam <ahmedhossam9226@gmail.com>
2024-09-16 07:42:05 +0300

    Time: 107 ms (78.64%) | Memory: 62.2 MB (78.59%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+function groupAnagrams(strs) {
+    const map = new Map();
+
+    for (let word of strs) {
+        let sortedWord = word.split('').sort().join('');
+        
+        if (!map.has(sortedWord)) {
+            map.set(sortedWord, []);
+        }
+        map.get(sortedWord).push(word);
+    }
+
+    return Array.from(map.values());
+}
